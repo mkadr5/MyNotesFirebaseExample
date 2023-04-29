8e7041554bc2ba10ae26dc431f3ad4ad15b869d5
mkadr5 <muhammetkadir52@gmail.com>
2023-04-29 13:49:25 +0300

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Muhammet Kadir on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
